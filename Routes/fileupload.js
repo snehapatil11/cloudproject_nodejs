b2f1312d54b2c799cabe7536416dd0303a8892df
sneha <sneha.magdum11@gmail.com>
2019-10-06 21:31:55 -0700

    s3 file upload and delete

---
+++ Routes/fileupload.js
@@ -0,0 +1,62 @@
+var AWS = require('aws-sdk')
+var multer = require('multer')
+var multerS3 = require('multer-s3')
+const express = require("express")
+const bodyParser = require('body-parser')
+const router = express.Router()
+router.use(bodyParser.json())
+const awsConfigJson = require("../Config/awsconfig")
+
+let awsConfig = {
+  "region": awsConfigJson.region,
+  "accessKeyId": awsConfigJson.accessKeyId, 
+  "secretAccessKey": awsConfigJson.secretAccessKey
+ };
+
+AWS.config.update(awsConfig);
+var s3 = new AWS.S3();
+
+var upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'cloudstoragebucket1',
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      metadata: function (req, file, cb) {
+        cb(null, {fieldName: 'testfile'});
+      },
+      key: function (req, file, cb) {
+        cb(null, file.originalname.toString())
+      }
+    })
+});
+
+const singleUpload = upload.single('file');
+
+router.post('/fileupload', function(req, res) {
+
+  singleUpload(req, res, function(err) {
+    if (err) {
+      return res.status(422).send({errors: [{title: 'File Upload Error', detail: err.message}] });
+    }
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    return res.json({'imageUrl' : req.file.location, 'fileName': req.file.key});
+  });
+});
+
+router.post('/filedelete', function(req, res){
+  console.log(req.body);
+  var params = {
+    Bucket: 'cloudstoragebucket1',
+    Key: req.body.fileName
+  };
+  s3.deleteObject(params, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const AWS = require('aws-sdk');
+var AWS = require('aws-sdk')
+var express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const fileRoutes = require("./Routes/fileupload")
+const awsConfigJson = require("./Config/awsdynamodbconfig")
+var app = express();
+
+let awsConfig = {
+  "region": awsConfigJson.region,
+ "endpoint": awsConfigJson.endpoint,
+ "accessKeyId": awsConfigJson.accessKeyId, 
+ "secretAccessKey": awsConfigJson.secretAccessKey
+};
 app.use(cors());
+app.use("/api", fileRoutes);
 
 
 AWS.config.update(awsConfig);
-
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
+var s3 = new AWS.S3();
 
 app.use(bodyParser.json({ type: 'application/json' }));
 
 app.listen(4001, () => {
     console.log("Server running on port 4001");
 })
-
-
 app.get('/', function (req, res) {
 
   res.send('Hello World!')
@@ -26,7 +33,7 @@ app.get('/', function (req, res) {
 
 app.get('/users', function (req, res) {
     const params = {  
-      TableName: "users"
+      TableName: "storageFiles"
     }    
     dynamoDb.scan(params, (error, result) => {
   
@@ -35,20 +42,21 @@ app.get('/users', function (req, res) {
         return res.status(400).json({ error: 'Could not get user' });  
       }
   
-      if (result) { return res.json({result}); } 
+      if (result) { 
+        //console.log(result.Items);
+        return res.json(result.Items);
+      } 
       else { return res.status(404).json({ error: "User not found" }); }  
     });
   
-  })
+})
 
-  app.post('/postusers', function(req, res){
+app.post('/postusers', function(req, res){
     const params = {  
-      TableName: "users",
-      Item: {
-        "email":"abcdde1@gmail.com",
-        "name":"sneha1"
-      }
+      TableName: "storageFiles",
+      Item: req.body
     }
+    console.log(params);
     dynamoDb.put(params, (error, result) =>{
       
       if(error){
@@ -57,11 +65,29 @@ app.get('/users', function (req, res) {
         return res.status(400).json({ error: 'Could not insert user' });
       }
       if(result){
+        console.log(result);
         return res.json({result});
       }
       else
       return res.status(400).json({ error: 'Could not insert user1' });  
     })
 
-  })
+})
 
+app.delete('/deletefiledata/:fileId',function(req, res){
+  const params = {  
+    TableName: "storageFiles",
+    Key: 
+    {
+      "Id": req.params.fileId      
+    }
+  }
+  console.log(params);
+  dynamoDb.delete(params, function(err, data) {
+    if (err) {
+        console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        res.json(data);
+    }
+  });
+})

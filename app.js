8fd85989dda01eaa90b9642a734d11b74dfcb6cd
sneha <sneha.magdum11@gmail.com>
2019-10-12 11:17:27 -0700

    changes for codepipeline

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var AWS = require('aws-sdk');
-var express = require("express");
-var router = express.Router();
+var AWS = require('aws-sdk')
+var express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const fileRoutes = require("./Routes/fileupload")
 const awsConfigJson = require("./Config/awsdynamodbconfig")
+require('dotenv').config();
 var app = express();
 
 let awsConfig = {
@@ -11,28 +14,82 @@ let awsConfig = {
  "secretAccessKey": process.env.secretAccessKey
 };
 
+app.use(cors());
+app.use("/api", fileRoutes);
+
+
 AWS.config.update(awsConfig);
-let docClient = new AWS.DynamoDB.DocumentClient();
-var dynamodb = new AWS.DynamoDB();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+var s3 = new AWS.S3();
 
-app.get("/read",(req, res, next)=>{
-    const params = {
-        TableName: "storageFiles"
-    };    
-    
-    docClient.scan(params, function(err, data) {
-        if (err) {
-          res.send({
-            success: false,
-            message: 'Error: Server error'
-          });
-        } else {
-          res.send("hello world");
-        }
-      });
+app.use(bodyParser.json({ type: 'application/json' }));
 
-});
+app.listen(4001, () => {
+    console.log("Server running on port 4001");
+})
+app.get('/', function (req, res) {
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+  res.send('Hello World!')
+
+})
+
+app.get('/users', function (req, res) {
+    const params = {  
+      TableName: "storageFiles"
+    }    
+    dynamoDb.scan(params, (error, result) => {
+  
+      if (error) {  
+        console.log(error);  
+        return res.status(400).json({ error: 'Could not get user' });  
+      }
+  
+      if (result) { 
+        //console.log(result.Items);
+        return res.json(result.Items);
+      } 
+      else { return res.status(404).json({ error: "User not found" }); }  
+    });
+  
+})
+
+app.post('/postusers', function(req, res){
+    const params = {  
+      TableName: "storageFiles",
+      Item: req.body
+    }
+    console.log(params);
+    dynamoDb.put(params, (error, result) =>{
+      
+      if(error){
+        console.log(error);
+        console.log(params);
+        return res.status(400).json({ error: 'Could not insert user' });
+      }
+      if(result){
+        console.log(result);
+        return res.json({result});
+      }
+      else
+      return res.status(400).json({ error: 'Could not insert user1' });  
+    })
+
+})
+
+app.delete('/deletefiledata/:fileId',function(req, res){
+  const params = {  
+    TableName: "storageFiles",
+    Key: 
+    {
+      "Id": req.params.fileId      
+    }
+  }
+  console.log(params);
+  dynamoDb.delete(params, function(err, data) {
+    if (err) {
+        console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        res.json(data);
+    }
+  });
+})
--- server.js
+++ server.js
@@ -1,95 +1,95 @@
-var AWS = require('aws-sdk')
-var express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const fileRoutes = require("./Routes/fileupload")
-const awsConfigJson = require("./Config/awsdynamodbconfig")
-require('dotenv').config();
-var app = express();
+// var AWS = require('aws-sdk')
+// var express = require('express')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const fileRoutes = require("./Routes/fileupload")
+// const awsConfigJson = require("./Config/awsdynamodbconfig")
+// require('dotenv').config();
+// var app = express();
 
-let awsConfig = {
-  "region": process.env.region,
- "endpoint": process.env.endpoint,
- "accessKeyId": process.env.accessKeyId, 
- "secretAccessKey": process.env.secretAccessKey
-};
+// let awsConfig = {
+//   "region": process.env.region,
+//  "endpoint": process.env.endpoint,
+//  "accessKeyId": process.env.accessKeyId, 
+//  "secretAccessKey": process.env.secretAccessKey
+// };
 
-app.use(cors());
-app.use("/api", fileRoutes);
+// app.use(cors());
+// app.use("/api", fileRoutes);
 
 
-AWS.config.update(awsConfig);
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-var s3 = new AWS.S3();
+// AWS.config.update(awsConfig);
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// var s3 = new AWS.S3();
 
-app.use(bodyParser.json({ type: 'application/json' }));
+// app.use(bodyParser.json({ type: 'application/json' }));
 
-app.listen(4001, () => {
-    console.log("Server running on port 4001");
-})
-app.get('/', function (req, res) {
+// app.listen(4001, () => {
+//     console.log("Server running on port 4001");
+// })
+// app.get('/', function (req, res) {
 
-  res.send('Hello World!')
+//   res.send('Hello World!')
 
-})
+// })
 
-app.get('/users', function (req, res) {
-    const params = {  
-      TableName: "storageFiles"
-    }    
-    dynamoDb.scan(params, (error, result) => {
+// app.get('/users', function (req, res) {
+//     const params = {  
+//       TableName: "storageFiles"
+//     }    
+//     dynamoDb.scan(params, (error, result) => {
   
-      if (error) {  
-        console.log(error);  
-        return res.status(400).json({ error: 'Could not get user' });  
-      }
+//       if (error) {  
+//         console.log(error);  
+//         return res.status(400).json({ error: 'Could not get user' });  
+//       }
   
-      if (result) { 
-        //console.log(result.Items);
-        return res.json(result.Items);
-      } 
-      else { return res.status(404).json({ error: "User not found" }); }  
-    });
+//       if (result) { 
+//         //console.log(result.Items);
+//         return res.json(result.Items);
+//       } 
+//       else { return res.status(404).json({ error: "User not found" }); }  
+//     });
   
-})
+// })
 
-app.post('/postusers', function(req, res){
-    const params = {  
-      TableName: "storageFiles",
-      Item: req.body
-    }
-    console.log(params);
-    dynamoDb.put(params, (error, result) =>{
+// app.post('/postusers', function(req, res){
+//     const params = {  
+//       TableName: "storageFiles",
+//       Item: req.body
+//     }
+//     console.log(params);
+//     dynamoDb.put(params, (error, result) =>{
       
-      if(error){
-        console.log(error);
-        console.log(params);
-        return res.status(400).json({ error: 'Could not insert user' });
-      }
-      if(result){
-        console.log(result);
-        return res.json({result});
-      }
-      else
-      return res.status(400).json({ error: 'Could not insert user1' });  
-    })
+//       if(error){
+//         console.log(error);
+//         console.log(params);
+//         return res.status(400).json({ error: 'Could not insert user' });
+//       }
+//       if(result){
+//         console.log(result);
+//         return res.json({result});
+//       }
+//       else
+//       return res.status(400).json({ error: 'Could not insert user1' });  
+//     })
 
-})
+// })
 
-app.delete('/deletefiledata/:fileId',function(req, res){
-  const params = {  
-    TableName: "storageFiles",
-    Key: 
-    {
-      "Id": req.params.fileId      
-    }
-  }
-  console.log(params);
-  dynamoDb.delete(params, function(err, data) {
-    if (err) {
-        console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        res.json(data);
-    }
-  });
-})
+// app.delete('/deletefiledata/:fileId',function(req, res){
+//   const params = {  
+//     TableName: "storageFiles",
+//     Key: 
+//     {
+//       "Id": req.params.fileId      
+//     }
+//   }
+//   console.log(params);
+//   dynamoDb.delete(params, function(err, data) {
+//     if (err) {
+//         console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+//     } else {
+//         res.json(data);
+//     }
+//   });
+// })
